36421e74cb5e88baa03beb8765a6ce95676cce38
jrfnl <github_nospam@adviesenzo.nl>
2016-01-15 07:14:54 +0100

    Allow for the new wp_installing() function.
    Introduced in WP 4.4.
    See: https://core.trac.wordpress.org/changeset/34828

--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -822,7 +822,7 @@ function initialize_yoast_woocommerce_seo() {
 	}
 }
 
-if ( ! defined( 'WP_INSTALLING' ) || WP_INSTALLING === false ) {
+if ( ( function_exists( 'wp_installing' ) && wp_installing() === false ) || ( ! function_exists( 'wp_installing' ) && ( ! defined( 'WP_INSTALLING' ) || WP_INSTALLING === false ) ) ) {
 	add_action( 'plugins_loaded', 'initialize_yoast_woocommerce_seo', 20 );
 }
 
